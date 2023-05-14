fb8d734d27781a45dd1e01d911dee1ffac061449
anggriawanmr <anggriawanmr@gmail.com>
2023-05-14 20:28:50 +0700

    Editing article service :hammer:

--- src/services/article.js
+++ src/services/article.js
@@ -1,33 +1,27 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-  method: 'GET',
-  url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-  params: {
-    url: 'https://time.com/6266679/musk-ai-open-letter/',
-    length: '3',
-  },
-  headers: {
-    'X-RapidAPI-Key': '5161dac412msh88855767f4dda5ep17e12ajsn57ffbcd9f2de',
-    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com',
-  },
-};
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
   reducerPath: 'articleApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', 'KEY');
+      headers.set('X-RapidAPI-Key', rapidApiKey);
       headers.set(
         'X-RapidAPI-Host',
         'article-extractor-and-summarizer.p.rapidapi.com'
       );
+
+      return headers;
     },
   }),
   endpoints: (builder) => ({
     getSummary: builder.query({
-      query: (params) => `test`,
+      query: (params) =>
+        `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
   }),
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;
