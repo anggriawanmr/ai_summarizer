8bc1972d0b7f209f1c076673ebd7d7ecbb6f41e1
anggriawanmr <anggriawanmr@gmail.com>
2023-05-14 16:29:52 +0700

    Adding article service :sparkles:

--- src/services/store.js
+++ src/services/store.js
@@ -1,6 +1,12 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
+import { articleApi } from './article';
+
 export const store = configureStore({
+  reducer: {
+    [articleApi.reducerPath]: articleApi.reducer,
+  },
   reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(articleApi.middleware),
 });
