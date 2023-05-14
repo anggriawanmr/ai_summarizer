7ae41e6f76d3f55c7b57601940eaa2d07e69cd32
anggriawanmr <anggriawanmr@gmail.com>
2023-05-14 20:20:59 +0700

    Editing article service :hammer:

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,33 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const options = {
+  method: 'GET',
+  url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+  params: {
+    url: 'https://time.com/6266679/musk-ai-open-letter/',
+    length: '3',
+  },
+  headers: {
+    'X-RapidAPI-Key': '5161dac412msh88855767f4dda5ep17e12ajsn57ffbcd9f2de',
+    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com',
+  },
+};
+
 export const articleApi = createApi({
   reducerPath: 'articleApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'KEY');
+      headers.set(
+        'X-RapidAPI-Host',
+        'article-extractor-and-summarizer.p.rapidapi.com'
+      );
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) => `test`,
+    }),
+  }),
 });
